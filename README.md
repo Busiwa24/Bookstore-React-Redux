dcfeead8286773cdbf1617ec381b75d7b706fa84
Busiwa24 <busiwaliuma@gmail.com>
2021-10-20 17:04:14 +0200

    add working files

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Bookstore-React-Redux
+# Bookstore
+
+> This is a React JS application that allows you to add books and track your reading progress.
+
+
+## Built with
+
+- JavaScript/React
+- React Router
+- JSX
+- CSS
+## Setup
+
+- Get the link of the repository: `git@github.com:mmatongo/bookstore.git`
+- Clone it as `git@github.com:mmatongo/bookstore.git` on a Terminal
+
+## Usage
+
+- Run `npm install` on a Terminal to install the modules
+- Run `yarn start` on a Terminal to start the server 
+- Open [http://localhost:3000/bookstore](http://localhost:3000/bookstore) to view it in the browser.
+
+## Author
+
+👤 **Busiwa Liuma**
+
+-GitHub: (https://github.com/Busiwa24) 
+-Twitter: (https://twitter.com/busiwaliuma) 
+-LinkedIn: (https://www.linkedin.com/in/busiwa-liuma-28385b80/>)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse
+- React and React Router 
+- Stack Overflow
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Header from './components/Nav';
+import Book from './components/Books';
+import NewBook from './components/NewBook';
+import Categories from './components/Categories';
+
+const books = [
+  {
+    id: 1,
+    category: 'Action',
+    name: 'Super woman',
+    author: 'Steven Spielberg',
+    percentage: '45%',
+    chapter: 'Chapter 14',
+  },
+  {
+    id: 2,
+    category: 'Science Fiction',
+    name: 'Journey to Mars',
+    author: 'Peter S. Miller',
+    percentage: '8%',
+    chapter: 'Chapter 3: "Limited Oxygen"',
+  },
+  {
+    id: 3,
+    category: 'Romance',
+    name: 'Somewhere between love and forever',
+    author: 'Mya Bridges',
+    percentage: '5%',
+    chapter: 'Introduction',
+  },
+];
+
+const App = () => {
+  const bookList = books.map((books) => <Book key={books.id} info={books} />);
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          { bookList }
+          <NewBook />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
